dbf868eb373e5cde17f53c09555b09f79da44005
amaity-digi <116334087+amaity-digi@users.noreply.github.com>
2023-08-31 23:58:11 +0530

    Error Handling

--- Basic/logical/index.js
+++ Basic/logical/index.js
@@ -116,5 +116,14 @@ function checkValue(number) {
 
 checkValue(num1);
 
-//sort() method
+//Write a JavaScript function that accepts two numbers as parameters and throws a custom 'Error' if the second number is zero.
+
+// function divideNumbers(n1, n2) {
+//     if (n2 === 0) {
+//       throw new Error('Error: Division by zero is not allowed.');
+//     }  
+//     return n1 / n2;
+//   }
+//   console.log(divideNumbers(8, 3));
+//   console.log(divideNumbers(8, 0));
 
